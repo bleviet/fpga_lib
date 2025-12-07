163073e9d76219505410d6e7d3098ca536398f0a
Bach Le Viet <bleviet@gmail.com>
2025-12-07 17:46:21 +0100

    Remove resources

--- fpga_lib/tests/parser/resources/vhdl/output/generated_neorv32_cpu_decompressor.vhd
+++
@@ -1,9 +0,0 @@
-library ieee;
-use ieee.std_logic_1164.all;
-
-entity neorv32_cpu_decompressor is
-    port (
-        instr_i : in std_ulogic_vector(15 downto 0);
-        instr_o : out std_ulogic_vector(31 downto 0)
-    );
-end entity neorv32_cpu_decompressor;
