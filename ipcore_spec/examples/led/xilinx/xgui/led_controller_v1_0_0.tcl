dfbd69d06f1a4ef4f1e2762b4b8bb2335fc3aa40
Bach Le Viet <bleviet@gmail.com>
2026-01-06 13:15:11 +0100

    fix: update Xilinx component.xml and XGUI templates to match Vivado format
    Updated templates to match Vivado IP Packager's exact formatting requirements
    to prevent Vivado from modifying files and causing errors.
    
    Component.xml changes:
    - Removed XML comments (<- Removed tracked GHDL -->)
    - Reformatted portMaps to multi-line format (separate lines for each element)
    - Removed spirit:format="long" from addressOffset
    - Added writeValueConstraint and testable elements to all fields
    - Added viewChecksum parameters to all views (synthesis, simulation, xpgui)
    - Added CHECKSUM_* userFileType to top-level VHDL file and XGUI file
    - Removed spirit:format="string" from Component_Name parameter
    - Added spirit:resolve="user" to generic parameters
    - Added coreRevision and coreCreationDateTime to coreExtensions
    - Added packagingInfo section with Vivado version and checksums
    
    XGUI template changes:
    - Added Page_0 grouping for parameters with -parent option
    - Removed Component_Name update/validate procedures
    - Removed all update_MODELPARAM_VALUE procedures
    - Added proper spacing between procedure blocks
    
    These changes ensure Vivado will not modify the generated files,
    preventing validation errors and maintaining consistency.

--- ipcore_spec/examples/led/xilinx/xgui/led_controller_v1_0_0.tcl
+++ ipcore_spec/examples/led/xilinx/xgui/led_controller_v1_0_0.tcl
@@ -1,19 +1,13 @@
 # Definitional proc to organize widgets for parameters.
 proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
-  ipgui::add_param $IPINST -name "NUM_LEDS"
-  ipgui::add_param $IPINST -name "PWM_BITS"
-  ipgui::add_param $IPINST -name "BLINK_COUNTER_WIDTH"
+  #Adding Page
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  ipgui::add_param $IPINST -name "NUM_LEDS" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "PWM_BITS" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "BLINK_COUNTER_WIDTH" -parent ${Page_0}
 
-}
 
-proc update_PARAM_VALUE.Component_Name { PARAM_VALUE.Component_Name } {
-	# Procedure called to update Component_Name when any of the dependent parameters in the arguments change
-}
-
-proc validate_PARAM_VALUE.Component_Name { PARAM_VALUE.Component_Name } {
-	# Procedure called to validate Component_Name
-	return true
 }
 
 proc update_PARAM_VALUE.NUM_LEDS { PARAM_VALUE.NUM_LEDS } {
@@ -24,6 +18,7 @@ proc validate_PARAM_VALUE.NUM_LEDS { PARAM_VALUE.NUM_LEDS } {
 	# Procedure called to validate NUM_LEDS
 	return true
 }
+
 proc update_PARAM_VALUE.PWM_BITS { PARAM_VALUE.PWM_BITS } {
 	# Procedure called to update PWM_BITS when any of the dependent parameters in the arguments change
 }
@@ -32,6 +27,7 @@ proc validate_PARAM_VALUE.PWM_BITS { PARAM_VALUE.PWM_BITS } {
 	# Procedure called to validate PWM_BITS
 	return true
 }
+
 proc update_PARAM_VALUE.BLINK_COUNTER_WIDTH { PARAM_VALUE.BLINK_COUNTER_WIDTH } {
 	# Procedure called to update BLINK_COUNTER_WIDTH when any of the dependent parameters in the arguments change
 }
@@ -39,22 +35,4 @@ proc update_PARAM_VALUE.BLINK_COUNTER_WIDTH { PARAM_VALUE.BLINK_COUNTER_WIDTH } 
 proc validate_PARAM_VALUE.BLINK_COUNTER_WIDTH { PARAM_VALUE.BLINK_COUNTER_WIDTH } {
 	# Procedure called to validate BLINK_COUNTER_WIDTH
 	return true
-}
-
-proc update_MODELPARAM_VALUE.Component_Name { MODELPARAM_VALUE.Component_Name PARAM_VALUE.Component_Name } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.Component_Name}] ${MODELPARAM_VALUE.Component_Name}
-}
-
-proc update_MODELPARAM_VALUE.NUM_LEDS { MODELPARAM_VALUE.NUM_LEDS PARAM_VALUE.NUM_LEDS } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.NUM_LEDS}] ${MODELPARAM_VALUE.NUM_LEDS}
-}
-proc update_MODELPARAM_VALUE.PWM_BITS { MODELPARAM_VALUE.PWM_BITS PARAM_VALUE.PWM_BITS } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.PWM_BITS}] ${MODELPARAM_VALUE.PWM_BITS}
-}
-proc update_MODELPARAM_VALUE.BLINK_COUNTER_WIDTH { MODELPARAM_VALUE.BLINK_COUNTER_WIDTH PARAM_VALUE.BLINK_COUNTER_WIDTH } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.BLINK_COUNTER_WIDTH}] ${MODELPARAM_VALUE.BLINK_COUNTER_WIDTH}
 }
